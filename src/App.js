fdc733f66bde41718301eb103fdbfce95c2228f0
eliftugull <eliftugull@gmail.com>
2023-12-07 19:29:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Pokegame from './Pokegame'
+import './Pokecard.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Pokemon Go Card Game</h1>
+      <Pokegame />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
